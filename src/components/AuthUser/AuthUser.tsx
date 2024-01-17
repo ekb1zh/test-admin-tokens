4f2f87f4d9b4d3584f3f1f2c109ba14ddcb086f1
ekb1zh <ekb1zh@gmail.com>
2024-01-17 10:48:34 +0100

    Add AuthUser component

---
+++ src/components/AuthUser/AuthUser.tsx
@@ -0,0 +1,30 @@
+import { forwardRef } from 'react'
+
+import Icon from 'src/components/Icon'
+import Typography from 'src/components/Typography'
+import { AuthUserProps } from 'src/components/AuthUser/types'
+import styles from 'src/components/AuthUser/AuthUser.module.scss'
+
+export const AuthUser = forwardRef<HTMLDivElement, AuthUserProps>(
+  (props, ref) => {
+    return (
+      <div ref={ref} className={styles.Root}>
+        <Icon.DefaultAvatar />
+
+        <div className={styles.Container}>
+          <Typography.Text
+            resolution='desktop'
+            type='body-xs-regular'
+            className={styles.Text1}
+          >
+            Вы авторизованы
+          </Typography.Text>
+
+          <Typography.Text resolution='desktop' type='body-s-medium'>
+            Администратор
+          </Typography.Text>
+        </div>
+      </div>
+    )
+  },
+)
---
+++ src/components/AuthUser/index.ts
@@ -0,0 +1,2 @@
+export { AuthUser as default } from 'src/components/AuthUser/AuthUser'
+export * from 'src/components/AuthUser/types'
