017a3d996335c9208d2d2d2737260cf4d10a3aab
ekb1zh <ekb1zh@gmail.com>
2024-01-19 02:38:39 +0100

    Update Pugination component

--- src/components/Table/Pagination/Pagination.tsx
+++ src/components/Table/Pagination/Pagination.tsx
@@ -1,126 +1,132 @@
-import { forwardRef, useCallback, useMemo, useState } from 'react'
+import { forwardRef, useCallback, useMemo } from 'react'
 import clsx from 'clsx'
-import isEqual from 'lodash/isEqual'
 
 import Typography from 'src/components/Typography'
 import Icon from 'src/components/Icon'
-import { usePaginationState } from 'src/components/Table/Pagination/hooks'
-import { PaginationState } from 'src/components/Table/Pagination/types'
+import { PaginationProps } from 'src/components/Table/Pagination/types'
 import styles from 'src/components/Table/Pagination/Pagination.module.scss'
 
-export const Pagination = forwardRef<HTMLDivElement>((_, ref) => {
-  /*
-    Temp start
-  */
-  const [state, setState] = useState<PaginationState>({
-    current: 0,
-    firstInRange: 0,
-    lengthOfRange: 4,
-    lengthTotal: 20,
-  })
+export const Pagination = forwardRef<HTMLDivElement, PaginationProps>(
+  ({ state, setState }, ref) => {
+    const { current, firstInRange, lengthOfRange, lengthTotal } = state
 
-  const updateState = (next: PaginationState) =>
-    setState((prev) => (isEqual(next, prev) ? prev : next))
-  /*
-    Temp end
-  */
+    const first = 0
+    const last = lengthTotal - 1
+    const lastInRange = firstInRange + lengthOfRange - 1
 
-  const {
-    state: { current, firstInRange, lengthOfRange, lengthTotal },
-    goToIndex,
-  } = usePaginationState(state, updateState)
+    const goToIndex = useCallback(
+      (index: number) => {
+        const nextState = { ...state }
 
-  const renderNumberButton = useCallback(
-    (index: number, isSelected: boolean) => (
-      <button
-        key={index}
-        type='button'
-        className={clsx(
-          styles.Button,
-          styles.Button_NumberButton,
-          isSelected && styles.Button_NumberButton_selected,
-        )}
-        onClick={() => goToIndex(index)}
-        disabled={isSelected}
-      >
-        <Typography.Text type='body-m-medium'>{index + 1}</Typography.Text>
-      </button>
-    ),
-    [goToIndex],
-  )
+        if (index >= first && index <= last) {
+          nextState.current = index
+        }
 
-  const renderDots = useCallback(
-    () => (
-      <div className={styles.Dots}>
-        <Typography.Text type='body-m-medium'>....</Typography.Text>
-      </div>
-    ),
-    [],
-  )
+        if (index > lastInRange) {
+          nextState.firstInRange += index - lastInRange
+        }
 
-  const range = useMemo(() => {
-    const numberButtons = Array.from({
-      length: Math.min(lengthTotal, lengthOfRange),
-    }).map((_, index) =>
-      renderNumberButton(
-        firstInRange + index,
-        current === firstInRange + index,
-      ),
+        if (index < firstInRange) {
+          nextState.firstInRange = index
+        }
+
+        setState(nextState)
+      },
+      [firstInRange, last, lastInRange, state, setState],
     )
 
-    const isDotsLeft = current > Math.floor((lengthTotal - 1) / 2)
+    const renderNumberButton = useCallback(
+      (index: number, isSelected: boolean) => (
+        <button
+          key={index}
+          type='button'
+          className={clsx(
+            styles.Button,
+            styles.Button_NumberButton,
+            isSelected && styles.Button_NumberButton_selected,
+          )}
+          onClick={() => goToIndex(index)}
+          disabled={isSelected}
+        >
+          <Typography.Text type='body-m-medium'>{index + 1}</Typography.Text>
+        </button>
+      ),
+      [goToIndex],
+    )
 
-    if (lengthTotal > lengthOfRange) {
-      return [
-        isDotsLeft && (
-          <>
-            {renderNumberButton(0, current === 0)}
-            {renderDots()}
-          </>
+    const renderDots = useCallback(
+      () => (
+        <div className={styles.Dots}>
+          <Typography.Text type='body-m-medium'>....</Typography.Text>
+        </div>
+      ),
+      [],
+    )
+
+    const range = useMemo(() => {
+      const numberButtons = Array.from({
+        length: Math.min(lengthTotal, lengthOfRange),
+      }).map((_, index) =>
+        renderNumberButton(
+          firstInRange + index,
+          current === firstInRange + index,
         ),
+      )
 
-        ...numberButtons,
+      const isDotsLeft = current > Math.floor((lengthTotal - 1) / 2)
 
-        !isDotsLeft && (
-          <>
-            {renderDots()}
-            {renderNumberButton(lengthTotal - 1, current === lengthTotal - 1)}
-          </>
-        ),
-      ]
-    } else {
-      return numberButtons
-    }
-  }, [
-    current,
-    firstInRange,
-    lengthOfRange,
-    lengthTotal,
-    renderDots,
-    renderNumberButton,
-  ])
+      if (lengthTotal > lengthOfRange) {
+        return [
+          isDotsLeft && (
+            <>
+              {renderNumberButton(0, current === 0)}
+              {renderDots()}
+            </>
+          ),
 
-  return (
-    <div ref={ref} className={styles.Root}>
-      <button
-        type='button'
-        className={clsx(styles.Button, styles.Button_ArrowButton)}
-        onClick={() => goToIndex(current - 1)}
-        disabled={current === 0}
-      >
-        <Icon.ArrowNarrowLeft />
-      </button>
+          ...numberButtons,
 
-      {range}
+          !isDotsLeft && (
+            <>
+              {renderDots()}
+              {renderNumberButton(lengthTotal - 1, current === lengthTotal - 1)}
+            </>
+          ),
+        ]
+      } else {
+        return numberButtons
+      }
+    }, [
+      current,
+      firstInRange,
+      lengthOfRange,
+      lengthTotal,
+      renderDots,
+      renderNumberButton,
+    ])
 
-      <button
-        type='button'
-        className={clsx(styles.Button, styles.Button_ArrowButton)}
-        onClick={() => goToIndex(current + 1)}
-        disabled={current === lengthTotal - 1}
-      >
-        <Icon.ArrowNarrowRight />
-      </button>
-    </div>
-  )
-})
+    return (
+      <div ref={ref} className={styles.Root}>
+        <button
+          type='button'
+          className={clsx(styles.Button, styles.Button_ArrowButton)}
+          onClick={() => goToIndex(current - 1)}
+          disabled={current === 0}
+        >
+          <Icon.ArrowNarrowLeft />
+        </button>
+
+        {range}
+
+        <button
+          type='button'
+          className={clsx(styles.Button, styles.Button_ArrowButton)}
+          onClick={() => goToIndex(current + 1)}
+          disabled={current === lengthTotal - 1}
+        >
+          <Icon.ArrowNarrowRight />
+        </button>
+      </div>
+    )
+  },
+)
